48ecbdcf7e1e19bf73c5d83ef1101a6300e0dc2f
NaokiM03 <naokim03.lib+dev@gmail.com>
2023-03-21 01:23:37 +0900

    Add starttime

---
+++ src/lib.rs
@@ -0,0 +1,58 @@
+use std::{fs, io::Result};
+
+#[allow(non_camel_case_types)]
+type c_int = i32;
+#[allow(non_camel_case_types)]
+type c_long = i64;
+#[allow(non_camel_case_types)]
+type c_ulonglong = u64;
+
+const _SC_CLK_TCK: c_int = 2;
+
+extern "C" {
+    fn sysconf(name: c_int) -> c_long;
+}
+
+pub fn starttime() -> Result<u64> {
+    // awk '{print $22}' /proc/self/stat
+    let starttime = fs::read_to_string("/proc/self/stat")?
+        .split_ascii_whitespace()
+        .nth(22 - 1)
+        .expect("Failed to get starttime")
+        .parse::<c_ulonglong>()
+        .expect("Failed to parse starttime");
+
+    let clock_ticks = unsafe { sysconf(_SC_CLK_TCK) as u64 };
+
+    let starttime = starttime / clock_ticks;
+    Ok(starttime)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_starttime() {
+        let result = starttime().unwrap();
+
+        let expect = {
+            let output = std::process::Command::new("awk")
+                .args(["{print $22}", "/proc/self/stat"])
+                .output()
+                .unwrap();
+            let stdout = output.stdout;
+            let starttime = stdout
+                .iter()
+                .filter(|&&x| x != '\n' as u8)
+                .map(|&x| x as char)
+                .collect::<String>();
+
+            let clock_ticks = unsafe { sysconf(_SC_CLK_TCK) as u64 };
+
+            starttime.parse::<u64>().unwrap() / clock_ticks
+        };
+
+        assert_eq!(result, expect);
+    }
+}
